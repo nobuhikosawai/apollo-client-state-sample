ca494f7dcfd1ee0311a29d96c8ab9b49e8f24b31
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2019-02-03 22:00:40 +0900

    Add server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+const port = 4000;
+
+app.listen({ port }, () => 
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
+);
